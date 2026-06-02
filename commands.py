fdf4e0df2a02d44f010ba14e33e8ca6cf55fb248
Felarm <kanafjev@gmail.com>
2026-06-02 17:31:47 +0300

    automation testing

--- commands.py
+++ commands.py
@@ -1,7 +1,11 @@
 import asyncio
+import json
 from argparse import ArgumentParser
 
+from loguru import logger
+
 from database import async_session_maker
+from main import app
 from models.user import User
 from services.security import PasswordService, JWTService
 
@@ -17,7 +21,14 @@ async def create_service_user(username: str, password: str) -> None:
         db_session.add(new_service_user)
         await db_session.commit()
         service_token_payload = JWTService._create_service_payload(new_service_user)
-        print(JWTService._encode_jwt(service_token_payload))
+        logger.info(JWTService._encode_jwt(service_token_payload))
+
+
+def export_contracts():
+    with open("openapi.json", "w") as f:
+        json.dump(app.openapi(), f, indent=2)
+    logger.info("exported schemas to openapi.json")
+
 
 
 def main():
@@ -28,9 +39,13 @@ def main():
     service_user_p.add_argument("--username", required=True, help="Username")
     service_user_p.add_argument("--password", required=True, help="Password")
 
+    subparsers.add_parser("export_openapi")
+
     args = parser.parse_args()
     if args.command == "create_service_user":
         asyncio.run(create_service_user(args.username, args.password))
+    elif args.command == "export_openapi":
+        export_contracts()
 
 
 if __name__ == "__main__":
